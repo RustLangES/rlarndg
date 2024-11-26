49f4169378f837fde7409b07da724c7fbc59f58a
stifskere <stifskere@gmail.com>
2024-11-26 03:45:30 +0100

    feat: use litcrypt

--- src/helpers/database/connection.rs
+++ src/helpers/database/connection.rs
@@ -2,15 +2,11 @@ use std::sync::OnceLock;
 use std::path::Path;
 use sqlx::{migrate::{MigrateError, Migrator}, postgres::PgPoolOptions, Error as SqlxError, Pool, Postgres};
 use thiserror::Error;
-use dotenv::{var, Error as VarError};
 
 static CONNECTION: OnceLock<Pool<Postgres>> = OnceLock::new();
 
 #[derive(Error, Debug)]
 pub enum DbConnectionError {
-    #[error("The DATABASE_URL environment variable couldn't be found.")]
-    Environment(#[from] VarError),
-
     #[error("{0:#}")]
     Connection(#[from] SqlxError),
 
@@ -25,7 +21,7 @@ pub async fn get_db_connection<'r>() -> Result<&'r Pool<Postgres>, DbConnectionE
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(&var("DATABASE_URL")?)
+        .connect(&lc!("DATABASE_URL"))
         .await?;
 
     Migrator::new(Path::new("./migrations"))
